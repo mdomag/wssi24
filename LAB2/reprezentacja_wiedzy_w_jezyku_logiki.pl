6ed9dd2ebf358f70b412941769a140979fa0ec00
mdomag <126866195+mdomag@users.noreply.github.com>
2024-04-10 21:51:05 +0200

    Add files via upload

---
+++ LAB2/reprezentacja_wiedzy_w_jezyku_logiki.pl
@@ -0,0 +1,54 @@
+:- discontiguous pozywienie/1.
+:- discontiguous zyje/2.
+:- discontiguous obywatel/2.
+%zadanie 1
+
+czlowiek(markus).
+obywatel(markus).
+wladca(cezar).
+proba_zamachu(markus, cezar).
+
+nienawidzi(X, Y) :- \+(lojalny(X, Y)).
+obywatel(X, rzym) :- obywatel(X, pompeje).
+obywatel(X, rzym) :- lojalny(X, cezar); nienawidzi(X, cezar).
+lojalny(X, Y) :- obywatel(X), wladca(Y), \+(proba_zamachu(X, Y)).
+
+%lojalny(Markus, Cezar) == false
+
+%zadanie 2
+
+pozywienie(jablka).
+pozywienie(kurczak).
+zabija(cos, kogos).
+%zabija(orzeszki, adam).
+trujace(X) :- je(Y, X), zabija(X, Y).
+pozywienie(X) :- \+trujace(X).
+lubi(jan, X) :- pozywienie(X).
+je(adam, orzeszki).
+je(basia, X) :- je(adam, X).
+
+% lubi(jan, orzeszki) == true
+% 
+%je(basia, X).
+%X = orzeszki
+
+%zadanie 3
+
+urodzil(markus, 40).
+obywatel(markus, pompeje).
+
+%sposob I
+
+zyje(X, 80) :- (obywatel(X, pompeje), urodzil(X, Z), Z>=80); (\+obywatel(X, pompeje)).
+
+%zyje(markus, 80) == false
+
+%sposob II
+%zyje(X, Y) :- urodzil(X, Z), Z > Y-150.
+
+%zyje(markus, 2021) == false
+
+
+
+
+
